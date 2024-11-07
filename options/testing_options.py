2d934de29fd37a121f6dea178430ee69d3502c2f
Tyrael09 <71233353+Tyrael09@users.noreply.github.com>
2024-11-07 15:30:38 +0100

    Update testing_options.py
    Formatted using black.

--- options/testing_options.py
+++ options/testing_options.py
@@ -1,42 +1,52 @@
 import argparse
 
+
 def str2bool(v):
-    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+    if v.lower() in ("yes", "true", "t", "y", "1"):
         return True
-    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+    elif v.lower() in ("no", "false", "f", "n", "0"):
         return False
     else:
-        raise argparse.ArgumentTypeError('Boolean value expected.')
+        raise argparse.ArgumentTypeError("Boolean value expected.")
 
 
-class TestOptions():
+class TestOptions:
     def __init__(self):
         """Reset the class; indicates the class hasn't been initailized"""
         self.initialized = False
+
     def initialize(self):
         parser = argparse.ArgumentParser()
-        parser.add_argument('--UseCUDA', help='Use CUDA?', type=str2bool, nargs='?', default=True)
-        parser.add_argument('--Mode', help='script mode', choices=['train', 'eval'], default='eval')
-        parser.add_argument('--ModelName', help='AE/MemAE', type=str, default='MemAE')
-        parser.add_argument('--ModelSetting',
-                            help='Conv3D/Conv3DSpar',
-                            type=str,
-                            default='Conv3DSpar')  # give the layer details later
-        parser.add_argument('--Seed', type=int, default=1)
-        parser.add_argument('--Dataset', help='Dataset', type=str, default='Cataract')
-        parser.add_argument('--ImgChnNum', help='image channel', type=int, default=1)
-        parser.add_argument('--FrameNum', help='frame num for VIDEO clip', type=int, default=16)
-        parser.add_argument('--BatchSize', help='BatchSize', type=int, default=1)
-        parser.add_argument('--MemDim', help='Memory Dimention', type=int, default=2000)
-        parser.add_argument('--EntropyLossWeight', help='EntropyLossWeight', type=float, default=0.0002)
-        parser.add_argument('--ShrinkThres', help='ShrinkThres', type=float, default=0.0025)
+        parser.add_argument("--UseCUDA", help="Use CUDA?", type=str2bool, nargs="?", default=True)
+        parser.add_argument("--Mode", help="script mode", choices=["train", "eval"], default="eval")
+        parser.add_argument("--ModelName", help="AE/MemAE", type=str, default="MemAE")
+        parser.add_argument(
+            "--ModelSetting", help="Conv3D/Conv3DSpar", type=str, default="Conv3DSpar"
+        )  # give the layer details later
+        parser.add_argument("--Seed", type=int, default=1)
+        parser.add_argument("--Dataset", help="Dataset", type=str, default="Cataract")
+        parser.add_argument("--ImgChnNum", help="image channel", type=int, default=1)
+        parser.add_argument("--FrameNum", help="frame num for VIDEO clip", type=int, default=16)  # TODO: change this
+        parser.add_argument("--BatchSize", help="BatchSize", type=int, default=1)
+        parser.add_argument("--MemDim", help="Memory Dimention", type=int, default=2000)  # TODO: change this
+        parser.add_argument("--EntropyLossWeight", help="EntropyLossWeight", type=float, default=0.0002)
+        parser.add_argument("--ShrinkThres", help="ShrinkThres", type=float, default=0.0025)
         ##
-        parser.add_argument('--ModelRoot', help='Path and name for trained model.', type=str, default='./models/')
-        parser.add_argument('--ModelFilePath', help='Path for a pretrained modle.', type=str, default='./models/model_MemAE_Conv3DSpar_Cataract_MemDim2000_EntW0.0002_ShrThres0.0025_Seed1_Non/MemAE_Conv3DSpar_Cataract_MemDim2000_EntW0.0002_ShrThres0.0025_Seed1_Non_epoch_0099_final.pt') # specify model
-        parser.add_argument('--DataRoot', help='DataPath', type=str, default='/local/scratch/hendrik/cataract_test_frames_downsized/') # not used in script
-        parser.add_argument('--OutRoot', help='Path for output', type=str, default='./results/') # TODO: update path between experiments
+        parser.add_argument("--ModelRoot", help="Path and name for trained model.", type=str, default="./models/")
+        parser.add_argument(
+            "--ModelFilePath",
+            help="Path for a pretrained modle.",
+            type=str,
+            default="models/model_MemAE_MemDim2000_FrameNum16_Overlap0.25/MemAE_MemDim2000_FrameNum16_Overlap0.25_epoch_0100_final.pt",
+        )  # specify model
+        parser.add_argument(
+            "--DataRoot", help="DataPath", type=str, default="/local/scratch/hendrik/cataract_test_frames_downsized/"
+        )  # not used in script
+        parser.add_argument(
+            "--OutRoot", help="Path for output", type=str, default="./results/4/"
+        )  # TODO: update path between experiments
         ##
-        parser.add_argument('--Suffix', help='Suffix', type=str, default='Non')
+        parser.add_argument("--Overlap", help="Overlap", type=float, default=1 / 4)  # TODO: change this
 
         self.initialized = True
         self.parser = parser
@@ -44,22 +54,22 @@ class TestOptions():
 
     def print_options(self, opt):
         # This function is adapted from 'cycleGAN' project.
-        message = ''
-        message += '----------------- Options ---------------\n'
+        message = ""
+        message += "----------------- Options ---------------\n"
         for k, v in sorted(vars(opt).items()):
-            comment = ''
+            comment = ""
             default = self.parser.get_default(k)
             if v != default:
-                comment = '\t[default: %s]' % str(default)
-            message += '{:>25}: {:<30}{}\n'.format(str(k), str(v), comment)
-        message += '----------------- End -------------------'
+                comment = "\t[default: %s]" % str(default)
+            message += "{:>25}: {:<30}{}\n".format(str(k), str(v), comment)
+        message += "----------------- End -------------------"
         print(message)
         self.message = message
 
     def parse(self, is_print):
         parser = self.initialize()
         opt = parser.parse_args()
-        if(is_print):
+        if is_print:
             self.print_options(opt)
         self.opt = opt
         return self.opt
