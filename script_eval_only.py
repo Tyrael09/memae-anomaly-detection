d9dd6b6225d71f41e5f592c32367ac56d04b481d
Tyrael09 <71233353+Tyrael09@users.noreply.github.com>
2024-11-12 17:34:23 +0100

    Create script_eval_only.py
    Useful for only running evaluation (metrics, plots) on existing model predictions.

---
+++ script_eval_only.py
@@ -0,0 +1,44 @@
+import data
+from data.merge_csvs_val import assign_labels_to_frames
+import utils
+from torchvision import transforms
+
+frame_root = "/local/scratch/hendrik/cataract_frames_downsized/"
+test_csv = "/local/scratch/hendrik/test_set.csv"
+res_path = "results/4/res_MemAE_MemDim2000_FrameNum16_Overlap0.25_ChNum3"  # TODO: fix 2nd part later
+chnum_in_ = 3
+
+# Frame transformations & data normalisation
+if chnum_in_ == 1:
+    norm_mean = [0.5]
+    norm_std = [0.5]
+    frame_trans = transforms.Compose(
+        [
+            transforms.Grayscale(num_output_channels=1),  # seems to be necessary. Why not train on 3 channels though?
+            transforms.ToTensor(),
+            transforms.Normalize(norm_mean, norm_std),
+        ]
+    )
+elif chnum_in_ == 3:
+    norm_mean = (0.5, 0.5, 0.5)
+    norm_std = (0.5, 0.5, 0.5)
+    frame_trans = transforms.Compose([transforms.ToTensor(), transforms.Normalize(norm_mean, norm_std)])
+
+video_dataset = data.MyDataset(
+    frame_root=frame_root,
+    csv_in=test_csv,
+    clip_len=16,
+    overlap=4,
+    split=[0, 1],
+    transform=frame_trans,
+)
+assign_labels_to_frames(
+    frame_csv_path="/local/scratch/hendrik/dataset_frame_lists.csv",  # updated by MyDataset, so wait for it to be initialised
+    label_csv_path="/local/scratch/hendrik/val_set.csv",
+    output_csv_path="/local/scratch/hendrik/merged_csv.csv",
+)
+eval_csv = "/local/scratch/hendrik/merged_csv.csv"
+
+
+## evaluation
+utils.my_eval_video(frame_root, res_path, eval_csv, normal=False, is_show=True)
