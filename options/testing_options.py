c975b6d9f6020899c649a5399dfcc29f69281a0c
Tyrael09 <71233353+Tyrael09@users.noreply.github.com>
2024-11-17 21:14:32 +0100

    Update testing_options.py
    Automatically extract MemDim, FrameNum and Overlap options from model path name for convenience.

--- options/testing_options.py
+++ options/testing_options.py
@@ -1,4 +1,5 @@
 import argparse
+import re
 
 
 def str2bool(v):
@@ -12,7 +13,7 @@ def str2bool(v):
 
 class TestOptions:
     def __init__(self):
-        """Reset the class; indicates the class hasn't been initailized"""
+        """Reset the class; indicates the class hasn't been initialized"""
         self.initialized = False
 
     def initialize(self):
@@ -22,10 +23,10 @@ class TestOptions:
         parser.add_argument("--ModelSetting", help="Conv3D/Conv3DSpar", type=str, default="Conv3DSpar")
         parser.add_argument("--Seed", type=int, default=1)
         parser.add_argument("--Dataset", help="Dataset", type=str, default="Cataract")
-        parser.add_argument("--ImgChnNum", help="image channel", type=int, default=1)  # TODO: change
-        parser.add_argument("--FrameNum", help="frame num for VIDEO clip", type=int, default=8)  # TODO: change
+        parser.add_argument("--ImgChnNum", help="image channel", type=int, default=1)  # Updated
+        parser.add_argument("--FrameNum", help="frame num for VIDEO clip", type=int, default=1)  # Updated
         parser.add_argument("--BatchSize", help="BatchSize", type=int, default=1)
-        parser.add_argument("--MemDim", help="Memory Dimention", type=int, default=8000)  # TODO: change
+        parser.add_argument("--MemDim", help="Memory Dimension", type=int, default=1)  # Updated
         parser.add_argument("--EntropyLossWeight", help="EntropyLossWeight", type=float, default=0.0002)
         parser.add_argument("--ShrinkThres", help="ShrinkThres", type=float, default=0.0025)
         parser.add_argument("--ModelRoot", help="model dir", type=str, default="/local/scratch/hendrik/models/")
@@ -33,33 +34,53 @@ class TestOptions:
             "--ModelFilePath",
             help="pretrained model",
             type=str,
-            default="/local/scratch/hendrik/models/model_MemAE_MemDim8000_FrameNum8_Overlap0.75_ChNum1/",  # TODO: change
+            default="/local/scratch/hendrik/models/model_MemAE_MemDim2000_FrameNum128_Overlap0.5/MemAE_MemDim2000_FrameNum128_Overlap0.5_epoch_0100_final.pt",
         )
         parser.add_argument("--OutRoot", help="Path for output", type=str, default="./results/4/")
-        parser.add_argument("--Overlap", help="Overlap", type=float, default=3 / 4)  # TODO: change
+        parser.add_argument("--Overlap", help="Overlap", type=float, default=1)  # Updated
 
         self.initialized = True
         self.parser = parser
         return parser
 
-    def print_options(self, opt):
-        # This function is adapted from 'cycleGAN' project.
-        message = ""
-        message += "----------------- Options ---------------\n"
-        for k, v in sorted(vars(opt).items()):
-            comment = ""
-            default = self.parser.get_default(k)
-            if v != default:
-                comment = "\t[default: %s]" % str(default)
-            message += "{:>25}: {:<30}{}\n".format(str(k), str(v), comment)
-        message += "----------------- End -------------------"
-        print(message)
-        self.message = message
+    def extract_model_info(self, model_file_path):
+        """Extract ImgChnNum, FrameNum, MemDim, and Overlap from the file path."""
+        regex = r"MemDim(\d+)_FrameNum(\d+)_Overlap([\d\.]+)"
+        match = re.search(regex, model_file_path)
+        if match:
+            memdim = int(match.group(1))
+            framenum = int(match.group(2))
+            overlap = float(match.group(3))
+            return memdim, framenum, overlap
+        else:
+            raise ValueError(f"Could not parse model information from path: {model_file_path}")
 
     def parse(self, is_print):
         parser = self.initialize()
         opt = parser.parse_args()
+
+        # Extract model-specific parameters from the file path
+        memdim, framenum, overlap = self.extract_model_info(opt.ModelFilePath)
+        opt.MemDim = memdim
+        opt.FrameNum = framenum
+        opt.Overlap = overlap
+
         if is_print:
             self.print_options(opt)
         self.opt = opt
         return self.opt
+
+    def print_options(self, opt):
+        message = ""
+        message += "----------------- Options ---------------\n"
+        for k, v in sorted(vars(opt).items()):
+            v = v if v is not None else "None"  # Replace None with a string representation
+            comment = ""
+            default = self.parser.get_default(k)
+            if v != default:
+                comment = f"\t[default: {default}]"
+            message += f"{k:>25}: {v:<30}{comment}\n"
+        message += "----------------- End -------------------"
+        print(message)
+        self.message = message
+
